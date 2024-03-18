3ac302806b773a3ac7be842f14dacf4b6345ea5a
pcranfield <pcranfield@gatekeeper-systems.com>
2024-03-18 14:33:16 -0700

    Upto 2.6

--- parser/parser.go
+++ parser/parser.go
@@ -55,6 +55,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -119,6 +121,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// TODO: We're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	// TODO: We're skipping the expression until we encounter a semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
